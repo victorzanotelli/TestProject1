c0a172f04dd4dcf6ed856fb4b81646468fb72d41
khameds <khameds@gmail.com>
2024-05-29 16:41:53 +0200

    [Create] Create img, scripts, styles folders and html files.

---
+++ scripts/include_html.js
@@ -0,0 +1,12 @@
+function includeNav() {
+   document.write(
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Catalogue</a></li>
+            <li><a href="#">Connexion</a></li>
+            <li><a href="#">Panier</a></li>
+        </ul>
+    </nav>
+   );  
+}
