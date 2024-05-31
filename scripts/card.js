1c5e838dcf9ce10330b0c3ea52d6e4724ad14e75
khameds <khameds@gmail.com>
2024-05-31 16:19:35 +0200

    [Update] Products are now load and display from the fake db

--- scripts/card.js
+++ scripts/card.js
@@ -1,18 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     const filters = document.querySelectorAll('.filter');
-    const catalogItems = document.querySelectorAll('.catalog-item');
+    let catalogItems = document.querySelectorAll('.catalog-item');
     const filterMenu = document.querySelector('.filter-menu');
     const toggleFiltersButton = document.getElementById('toggle-filters');
     const clearFiltersButton = document.getElementById('clear-filters');
     const cartItemsList = document.getElementById('cart-items');
     const addToCartButtons = document.querySelectorAll('.add-to-cart');
-
+    
+    console.log('%cscripts\card.js:10 catalogItems', 'color: #007acc;', catalogItems);
     function init(){
         sessionStorage.setItem("mykey","myvalue");
         clearFiltersButton.style.display = "none";
+
+    }
+
+    function loadCard(){
+        let cardContent = "";
+        for (let i = 0; i < bdd.products.length; i++) {
+            cardContent += "<div class=\"catalog-item\" data-category=\"" + bdd.products[i].brand + "\" data-price=\"" + bdd.products[i].priceRange +"\">";
+            cardContent += "<div class=\"product-name\">" + bdd.products[i].name +"</div>";
+            cardContent += "<div class=\"product-img\"><img src=\"" + bdd.products[i].img + "\" alt=\"" + bdd.products[i].name +"\"></div>";
+            cardContent += "<div class=\"product-desc\"><p>" + bdd.products[i].desc + "</p></div>";
+            cardContent += "<div class=\"product-price\"><div>Prix : </div><div>" + bdd.products[i].price + "€</div></div></div>";        
+        }
+        // console.log("html" + cardContent);
+        document.getElementById("catalog").innerHTML = cardContent;
     }
 
     init();
+    loadCard();
 
     toggleFiltersButton.addEventListener('click', () => {
         if (filterMenu.style.display === 'none' || filterMenu.style.display === '') {
@@ -44,13 +60,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function filterCatalog() {
         const activeFilters = Array.from(filters).filter(filter => filter.checked).map(filter => filter.dataset.filter);
-
+        catalogItems = document.querySelectorAll('.catalog-item');
+        console.log('%cscripts\card.js:63 catalogItems', 'color: #007acc;', catalogItems);
         catalogItems.forEach(item => {
             const itemCategories = item.dataset.category.split(' ');
             const itemPrices = item.dataset.price.split(' ');
             const isCategoryMatch = activeFilters.some(filter => itemCategories.includes(filter));
             const isPriceMatch = activeFilters.some(filter => itemPrices.includes(filter));
             
+            console.log('%cscripts\card.js:69 itemCategories', 'color: #007acc;', itemCategories);
+
             if(activeFilters.length === 0){
                 clearFiltersButton.style.display = "none";
             }
--- scripts/product.js
+++ scripts/product.js
@@ -16,15 +16,15 @@ class Bdd{
     constructor(){
         this.products = [];
 
-        this.products.push(new Product(1,"Asus Vivobook 15","Asus","img/1.jpg",1,469,1,"Découvrez des couleurs à votre image Reflétant l'audace de la jeunesse, le ASUS Vivobook vous aide à exprimer votre vraie nature. Doté de performances de pointe, d'une charnière ErgoLift innovante et d'un écran NanoEdge, il vous permet d'abattre quantité de travail.","descPlus"));
-        this.products.push(new Product(2,"Asus Vivobook 17","Asus","img/2.jpg",2,599,1,"Donnez de l'éclat à votre monde grâce au ASUS Vivobook 17X, l'ordinateur portable doté de nombreuses fonctionnalités et d'un écran brillant. Le Vivobook 17X facilite le travail, où que ce soit : tout a été amélioré, de son puissant processeur mobile AMD Ryzen™ 7 à sa charnière à 180°, en passant par ses couleurs modernes et son design géométrique épuré. Prenez un nouveau départ aujourd'hui avec le Vivobook 17X !","descPlus"));
-        this.products.push(new Product(3,"Acer Aspire 3","Acer","img/3.png",1,499,1,"l'Aspire 3 prêt à l'emploi avec les derniers processeurs AMD Ryzen™ série 70001 conçus pour le multitâche et la productivité.","descPlus"));
-        this.products.push(new Product(4,"Acer Aspire 5","Acer","img/4.jpg",2,699,1,"Les multiples options de couleurs de cet ordinateur portable tendance cachent une sélection de cartes graphiques et de processeurs puissants permettant de profiter de son écran au rapport corps/écran élevé.","descPlus"));
-        this.products.push(new Product(5,"Acer Nitro 5","Acer","img/5.jpg",3,1099,1,"Acer Nitro 5 AN517-55-54EK. Type de produit: Ordinateur portable, Format: Clapet. Famille de processeur: Intel® Core™ i5, Modèle de processeur: i5-12450H.","descPlus"));
-        this.products.push(new Product(6,"Dell Latitude 5400","Dell","img/6.jpg",0,380.90,1,"Windows 10 Pro., I5 8350U 1.7 GHZ - 8e Génération, Taille de l'écran : 14.1pouces, Webcam : Oui, Ecran tactile : Non","descPlus"));
-        this.products.push(new Product(7,"Dell Precision 7520","Dell","img/7.jpg",2,560.90,1,"Windows 10 Pro., I7 7820HQ 2.6 GHZ - 7e Génération, Taille de l'écran : 15.6pouces, Webcam : Oui, Ecran tactile : Non","descPlus"));
-        this.products.push(new Product(8,"Dell Latitude 5430","Dell","img/8.jpg",3,1172.10,1,"DELL Latitude 5430. Type de produit: Ordinateur portable, Format: Clapet. Famille de processeur: Intel® Core™ i5, Modèle de processeur: i5-1235U. Taille de l'écran: 35,6 cm (14'), Type HD: Full HD, Résolution de l'écran: 1920 x 1080 pixels.","descPlus"));
-        this.products.push(new Product(9,"Lenovo 82V700ALFR","Lenovo","img/9.jpg",0,229,1,"Couvrez tous vos besoins informatiques quotidiens tout en vous divertissant avec l’IdeaPad 1 (15.6, Intel). Équipé de processeurs Intel Celeron fiables et offrant une grande autonomie, vous pourrez consulter vos e-mails et publier de nouveaux messages sur les réseaux sociaux toute la journée, où bon vous semble.","descPlus"));
+        this.products.push(new Product(1,"Asus Vivobook 15","asus","img/1.jpg","price-low",469,1,"Découvrez des couleurs à votre image Reflétant l'audace de la jeunesse, le ASUS Vivobook vous aide à exprimer votre vraie nature. Doté de performances de pointe, d'une charnière ErgoLift innovante et d'un écran NanoEdge, il vous permet d'abattre quantité de travail.","descPlus"));
+        this.products.push(new Product(2,"Asus Vivobook 17","asus","img/2.jpg","price-medium",599,1,"Donnez de l'éclat à votre monde grâce au ASUS Vivobook 17X, l'ordinateur portable doté de nombreuses fonctionnalités et d'un écran brillant. Le Vivobook 17X facilite le travail, où que ce soit : tout a été amélioré, de son puissant processeur mobile AMD Ryzen™ 7 à sa charnière à 180°, en passant par ses couleurs modernes et son design géométrique épuré. Prenez un nouveau départ aujourd'hui avec le Vivobook 17X !","descPlus"));
+        this.products.push(new Product(3,"Acer Aspire 3","acer","img/3.png","price-low",499,1,"l'Aspire 3 prêt à l'emploi avec les derniers processeurs AMD Ryzen™ série 70001 conçus pour le multitâche et la productivité.","descPlus"));
+        this.products.push(new Product(4,"Acer Aspire 5","acer","img/4.jpg","price-medium",699,1,"Les multiples options de couleurs de cet ordinateur portable tendance cachent une sélection de cartes graphiques et de processeurs puissants permettant de profiter de son écran au rapport corps/écran élevé.","descPlus"));
+        this.products.push(new Product(5,"Acer Nitro 5","acer","img/5.jpg","price-high",1099,1,"Acer Nitro 5 AN517-55-54EK. Type de produit: Ordinateur portable, Format: Clapet. Famille de processeur: Intel® Core™ i5, Modèle de processeur: i5-12450H.","descPlus"));
+        this.products.push(new Product(6,"Dell Latitude 5400","dell","img/6.jpg","promo",380.90,1,"Windows 10 Pro., I5 8350U 1.7 GHZ - 8e Génération, Taille de l'écran : 14.1pouces, Webcam : Oui, Ecran tactile : Non","descPlus"));
+        this.products.push(new Product(7,"Dell Precision 7520","dell","img/7.jpg","price-medium",560.90,1,"Windows 10 Pro., I7 7820HQ 2.6 GHZ - 7e Génération, Taille de l'écran : 15.6pouces, Webcam : Oui, Ecran tactile : Non","descPlus"));
+        this.products.push(new Product(8,"Dell Latitude 5430","dell","img/8.jpg","price-high",1172.10,1,"DELL Latitude 5430. Type de produit: Ordinateur portable, Format: Clapet. Famille de processeur: Intel® Core™ i5, Modèle de processeur: i5-1235U. Taille de l'écran: 35,6 cm (14'), Type HD: Full HD, Résolution de l'écran: 1920 x 1080 pixels.","descPlus"));
+        this.products.push(new Product(9,"Lenovo 82V700ALFR","lenovo","img/9.jpg","price-promo",229,1,"Couvrez tous vos besoins informatiques quotidiens tout en vous divertissant avec l’IdeaPad 1 (15.6, Intel). Équipé de processeurs Intel Celeron fiables et offrant une grande autonomie, vous pourrez consulter vos e-mails et publier de nouveaux messages sur les réseaux sociaux toute la journée, où bon vous semble.","descPlus"));
     }
 
     getBrand(brandId){
@@ -64,10 +64,10 @@ class Bdd{
 
         return results;
     }
-};
+};  
 
 const bdd = new Bdd();
-
-console.log("nb match = " + bdd.filterProducts(["Dell","Lenovo"],[0,1,2,3]).length);
+if(sessionStorage.getItem("bdd") == null)
+    sessionStorage.setItem("bdd",bdd);
 
 
